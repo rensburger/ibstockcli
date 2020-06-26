01c700410c63c215ab573c235201cd4b23e2888c
rensburger <rvanrensburg@gmail.com>
2020-06-26 13:43:11 +0300

    Client number for paper trading account updated

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 {
     "Accounts" : [
-        { "Label": "pr", "Gateway": "127.0.0.1:4001", "Client": 0, "Paper": true },
+        { "Label": "pr", "Gateway": "127.0.0.1:4001", "Client": 7497, "Paper": true }, //7496 for live
         { "Label": "ib", "Gateway": "127.0.0.1:4002", "Client": 0, "Paper": false }
     ]
 }
